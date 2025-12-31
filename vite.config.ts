9bd6ce89b8fe396b4c98f9ca0ebf1d3868ee2412
sebastien1909 <sebastienconfrere6@gmail.com>
2025-12-31 12:26:27 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/portfolio/',
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
